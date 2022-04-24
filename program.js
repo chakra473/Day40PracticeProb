2fcb1ceb522f7d4e97de9a15196fdc8c881a4076
chakra473 <91408040+chakra473@users.noreply.github.com>
2022-04-24 23:04:45 +0530

    Added Prime Number In Range Program

---
+++ program.js
@@ -0,0 +1,15 @@
+var frange =prompt("Please enter first range");
+var lrange=prompt("Please enter last range")
+
+console.log("All prime number with in that range is:");
+for (i=frange;i<=lrange;i++){
+    var count=0;
+    for (var j=1;j<=i;j++){
+        if(i%j==0){
+            count++;
+        }
+    }
+    if(count==2){
+        console.log(i);
+    }
+}
